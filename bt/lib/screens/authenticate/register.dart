f97cf0fa1084f114b3d0e4fa5eed92288f118ea2
Azizi Keffli <Zeeykhm@gmail.com>
2023-03-23 12:59:53 +0800

    btfyp_23/3/23@12.59pm

--- bt/lib/screens/authenticate/register.dart
+++ bt/lib/screens/authenticate/register.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
 import '../../shared/constants.dart';
+import '../../shared/loading.dart';
 import '../services/auth.dart';
 
 class Register extends StatefulWidget {
-
   final Function toggleView;
   const Register({super.key, required this.toggleView});
 
@@ -13,9 +13,9 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
-
   final AuthServices _auth = AuthServices();
   final _formKey = GlobalKey<FormState>();
+  bool loading = false;
 
   //textfield state
   String email = '';
@@ -24,71 +24,92 @@ class _RegisterState extends State<Register> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.yellow[40],
-      appBar: AppBar(
-        backgroundColor: Colors.yellow[500],
-        elevation: 0.0,
-        title: const Text('Sign Up to Brunei Tourism'),
-        actions: <Widget>[
-          TextButton.icon(
-            onPressed: () {
-              widget.toggleView();
-            },
-            icon: const Icon(
-              Icons.person,
-            ), label: const Text('Log in'),
-          ),
-        ],
-      ),
-      body: Container(
-        padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: <Widget>[
-              const SizedBox(height: 20.0,),
-              TextFormField(
-                decoration: textInputDecoration.copyWith(hintText: 'Email'),
-                validator: (val) => val!.isEmpty ? 'Enter an Email' : null,
-                onChanged: (val) {
-                  setState(() => email = val);
-                },
-              ),
-              const SizedBox(height: 20.0,),
-              TextFormField(
-                decoration: textInputDecoration.copyWith(hintText: 'Password'),
-                obscureText: true,
-                validator: (val) => val!.length < 6 ? 'Enter a password more than 6 chars long' : null,
-                onChanged: (val){
-                  setState(() => password = val);
-                },
-              ),
-              const SizedBox(height: 20.0,),
-              ElevatedButton(
-                onPressed: () async { 
-                  if (_formKey.currentState!.validate()) {
-                    dynamic result = await _auth.registerWithEmailAndPassword(email, password);
-                    if (result == null) {
-                      setState(() => error = 'please enter a valid email');
-                    }
-                    
-                    // print(email);
-                    // print(password);
-                  }
-                 },
-                child: Text('Register',
-                style: TextStyle(color: Colors.white),)
+    return loading
+        ? const Loading()
+        : Scaffold(
+            backgroundColor: Colors.yellow[40],
+            appBar: AppBar(
+              backgroundColor: Colors.yellow[500],
+              elevation: 0.0,
+              title: const Text('Sign Up to Brunei Tourism'),
+              actions: <Widget>[
+                TextButton.icon(
+                  onPressed: () {
+                    widget.toggleView();
+                  },
+                  icon: const Icon(
+                    Icons.person,
+                  ),
+                  label: const Text('Log in'),
                 ),
-                SizedBox(height: 20.0,),
-                Text(
-                  error,
-                  style: TextStyle(color: Colors.red, fontSize: 14.0),
-                )
-            ],
-          ),
-        ),
-      ),
-    );
+              ],
+            ),
+            body: Container(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: <Widget>[
+                    const SizedBox(
+                      height: 20.0,
+                    ),
+                    TextFormField(
+                      decoration:
+                          textInputDecoration.copyWith(hintText: 'Email'),
+                      validator: (val) =>
+                          val!.isEmpty ? 'Enter an Email' : null,
+                      onChanged: (val) {
+                        setState(() => email = val);
+                      },
+                    ),
+                    const SizedBox(
+                      height: 20.0,
+                    ),
+                    TextFormField(
+                      decoration:
+                          textInputDecoration.copyWith(hintText: 'Password'),
+                      obscureText: true,
+                      validator: (val) => val!.length < 6
+                          ? 'Enter a password more than 6 chars long'
+                          : null,
+                      onChanged: (val) {
+                        setState(() => password = val);
+                      },
+                    ),
+                    const SizedBox(
+                      height: 20.0,
+                    ),
+                    ElevatedButton(
+                        onPressed: () async {
+                          if (_formKey.currentState!.validate()) {
+                            setState(() => loading = true);
+                            dynamic result = await _auth
+                                .registerWithEmailAndPassword(email, password);
+                            if (result == null) {
+                              error = 'please enter a valid email';
+                              loading = false;
+                            }
+
+                            // print(email);
+                            // print(password);
+                          }
+                        },
+                        child: Text(
+                          'Register',
+                          style: TextStyle(color: Colors.white),
+                        )),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    Text(
+                      error,
+                      style: TextStyle(color: Colors.red, fontSize: 14.0),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          );
   }
 }
--- bt/lib/screens/authenticate/sign_in.dart
+++ bt/lib/screens/authenticate/sign_in.dart
@@ -2,12 +2,11 @@ import 'package:bt/screens/services/auth.dart';
 import 'package:flutter/material.dart';
 
 import '../../shared/constants.dart';
+import '../../shared/loading.dart';
 
 class SignIn extends StatefulWidget {
-  
   final Function toggleView;
   const SignIn({super.key, required this.toggleView});
-  
 
   @override
   State<SignIn> createState() => _SignInState();
@@ -16,91 +15,112 @@ class SignIn extends StatefulWidget {
 class _SignInState extends State<SignIn> {
   final AuthServices _auth = AuthServices();
   final _formKey = GlobalKey<FormState>();
+  bool loading = false;
 
   //textfield state
   String email = '';
   String password = '';
   String error = '';
 
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.yellow[40],
-      appBar: AppBar(
-        backgroundColor: Colors.yellow[500],
-        elevation: 0.0,
-        title: const Text('Sign in to Brunei Tourism'),
-        actions: <Widget>[
-          TextButton.icon(
-            onPressed: () {
-              widget.toggleView();
-            },
-            icon: const Icon(
-              Icons.person,
-            ), label: const Text('Register'),
-          ),
-        ],
-      ),
-      body: Container(
-        padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: <Widget>[
-              const SizedBox(height: 20.0,),
-              TextFormField(
-                decoration: textInputDecoration.copyWith(hintText: 'Email'),
-                validator: (val) => val!.isEmpty ? 'Enter an Email' : null,
-                onChanged: (val) {
-                  setState(() => email = val);
-                },
-              ),
-              const SizedBox(height: 20.0,),
-              TextFormField(
-                decoration: textInputDecoration.copyWith(hintText: 'Password'),
-                obscureText: true,
-                validator: (val) => val!.length < 6 ? 'Enter a password more than 6 chars long' : null,
-                onChanged: (val){
-                  setState(() => password = val);
-                },
-              ),
-              const SizedBox(height: 20.0,),
-              ElevatedButton(
-                onPressed: () async { 
-                  if (_formKey.currentState!.validate()) {
-                    dynamic result = await _auth.signInWithEmailAndPassword(email, password);
-                    if (result == null) {
-                      setState(() => error = 'please enter a valid email');
-                    }
-                    
-                    // print(email);
-                    // print(password);
-                  }
-                 },
-                child: Text('Sign In',
-                style: TextStyle(color: Colors.white),)
+    return loading
+        ? const Loading()
+        : Scaffold(
+            backgroundColor: Colors.yellow[40],
+            appBar: AppBar(
+              backgroundColor: Colors.yellow[500],
+              elevation: 0.0,
+              title: const Text('Sign in to Brunei Tourism'),
+              actions: <Widget>[
+                TextButton.icon(
+                  onPressed: () {
+                    widget.toggleView();
+                  },
+                  icon: const Icon(
+                    Icons.person,
+                  ),
+                  label: const Text('Register'),
                 ),
-                SizedBox(height: 20.0,),
-                Text(
-                  error,
-                  style: TextStyle(color: Colors.red, fontSize: 14.0),
-                )
-            ],
-          ),
-        ),
-        // child: ElevatedButton(
-        //     onPressed: () async {
-        //       dynamic result = await _auth.signInAnon();
-        //       if (result == null) {
-        //         print('error signing in');
-        //       } else {
-        //         print('signed in');
-        //         print(result.uid);
-        //       }
-        //     },
-        //     child: const Text('Sign in Anon')),
-      ),
-    );
+              ],
+            ),
+            body: Container(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: <Widget>[
+                    const SizedBox(
+                      height: 20.0,
+                    ),
+                    TextFormField(
+                      decoration:
+                          textInputDecoration.copyWith(hintText: 'Email'),
+                      validator: (val) =>
+                          val!.isEmpty ? 'Enter an Email' : null,
+                      onChanged: (val) {
+                        setState(() => email = val);
+                      },
+                    ),
+                    const SizedBox(
+                      height: 20.0,
+                    ),
+                    TextFormField(
+                      decoration:
+                          textInputDecoration.copyWith(hintText: 'Password'),
+                      obscureText: true,
+                      validator: (val) => val!.length < 6
+                          ? 'Enter a password more than 6 chars long'
+                          : null,
+                      onChanged: (val) {
+                        setState(() => password = val);
+                      },
+                    ),
+                    const SizedBox(
+                      height: 20.0,
+                    ),
+                    ElevatedButton(
+                        onPressed: () async {
+                          if (_formKey.currentState!.validate()) {
+                            setState(() => loading = true);
+                            dynamic result = await _auth
+                                .signInWithEmailAndPassword(email, password);
+                            if (result == null) {
+                              error = 'please enter a valid email';
+                              loading = false;
+                            }
+
+                            // print(email);
+                            // print(password);
+                          }
+                        },
+                        child: Text(
+                          'Sign In',
+                          style: TextStyle(color: Colors.white),
+                        )),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    Text(
+                      error,
+                      style: TextStyle(color: Colors.red, fontSize: 14.0),
+                    )
+                  ],
+                ),
+              ),
+              // child: ElevatedButton(
+              //     onPressed: () async {
+              //       dynamic result = await _auth.signInAnon();
+              //       if (result == null) {
+              //         print('error signing in');
+              //       } else {
+              //         print('signed in');
+              //         print(result.uid);
+              //       }
+              //     },
+              //     child: const Text('Sign in Anon')),
+            ),
+          );
   }
 }
